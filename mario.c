bdff7780a7c4853d516907eee2b4f7bbfaf6cd02
memphishvd <memphishvd@users.noreply.github.com>
2020-04-19 07:40:04 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -8,24 +8,24 @@ int main(void)
    
    
     int h;
-
+// A loop to ensure the user inputs a positive integer between 1 and 8
     do 
         {
              h = get_int("Please Input The Required Pyramid Height:");
         }
     while (h<1 || h>8);
-    
-   for (int i=0; i<=h; i++)
+// First For Loop for rows    
+   for (int i=1; i<=h; i++)
         
         {   
-
+// Nested Loop-1 for printing empty spaces
             for (int j=h-i; j>0; j--) 
         
                  {
                         printf(" ");
 
                  }
-
+// Nested Loop-2 for printing #
             for (int j=0; j<i; j++) 
                   
                  {
