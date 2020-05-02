ae8b08d0c0d013407ad5aa4489b93ce57f30b067
memphishvd <memphishvd@users.noreply.github.com>
2020-05-02 11:42:59 +0000

    automated commit by submit50

--- 13.sql
+++ 13.sql
@@ -2,7 +2,7 @@ SELECT DISTINCT name FROM people
 WHERE name IS NOT "Kevin Bacon"
 AND id IN
                 (SELECT stars.person_id FROM stars WHERE stars.movie_id IN
-					(SELECT stars.movie_id FROM stars where stars.person_id IN
-						(Select people.id FROM people where name = "Kevin Bacon" and birth = 1958)
+					(SELECT stars.movie_id FROM stars WHERE stars.person_id IN
+						(SELECT people.id FROM people WHERE name = "Kevin Bacon" AND birth = 1958)
 					)
 				)
